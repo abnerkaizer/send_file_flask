b060b42704b1c71df15e0bae2f31ea54b8f53045
Abner Kaizer <contact@abnerkaizer.com>
2024-12-16 19:05:08 -0300

    Typo and added a new route for apis

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_wtf import FlaskForm
+from flask_wtf.csrf import CSRFProtect
 from wtforms import FileField, SubmitField
 from dotenv import load_dotenv
 from werkzeug.utils import secure_filename
 from wtforms.validators import InputRequired
+from flask import request
 import os
 
 load_dotenv()
@@ -11,16 +13,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("KEY")
 app.config['UPLOAD_FOLDER'] = os.getenv("FOLDER_PATH")
 
+
 class UploadFileForm(FlaskForm):
     file = FileField("File", validators=[InputRequired()])
     submit = SubmitField("Upload File")
+
+# Não aplicaremos CSRF em formulários específicos para APIs
+app.config['WTF_CSRF_ENABLED'] = False
 
 @app.route('/', methods=['GET'])
 def home():
     return '<h1>Flask Send File<h1>'
 
-@app.route('/uploud', methods=['POST'])
-def uploud():
+@app.route('/upload', methods=['GET','POST'])
+def upload():
     form = UploadFileForm()
     if form.validate_on_submit():
         file = form.file.data
@@ -37,11 +43,31 @@ def uploud():
             flash("File has been uploaded successfully!", "success")
         except Exception as e:
             flash(f"Error during file upload: {str(e)}", "danger")
-        return redirect(url_for('uploud'))
+        return redirect(url_for('upload'))
     else:
         if form.errors:
             flash("Failed to upload file. Please try again.", "danger")
     return render_template('index.html', form=form)
+
+@app.route('/api/upload', methods=['POST'])
+def api_upload():
+    if 'file' not in request.files:
+        return "No file part in the request.", 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file.", 400
+
+    try:
+        save_path = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            app.config['UPLOAD_FOLDER'],
+            secure_filename(file.filename)
+        )
+        file.save(save_path)
+        return "File has been uploaded successfully!", 200
+    except Exception as e:
+        return f"Error during file upload: {str(e)}", 500
 
 if __name__ == '__main__':
     app.run(debug=True)
